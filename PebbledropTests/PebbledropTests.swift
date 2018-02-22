f3877aa2004e0cd409bbbd3f7513a61b00bf8ca0
reidtc82 <reidtc82@gmail.com>
2018-02-22 13:08:51 -0800

    Initial Commit

---
+++ PebbledropTests/PebbledropTests.swift
@@ -0,0 +1,36 @@
+//
+//  PebbledropTests.swift
+//  PebbledropTests
+//
+//  Created by Reid Case on 2/22/18.
+//  Copyright © 2018 Reid Case. All rights reserved.
+//
+
+import XCTest
+@testable import Pebbledrop
+
+class PebbledropTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
